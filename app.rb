c4757cc699eabea89bb36ad06679b0b431d3130d
Jack Hubert <jphubert93@gmail.com>
2020-12-10 15:36:29 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,5 +1,3 @@
-require 'pry'
-
 class Company
     attr_accessor :name, :size
 
@@ -23,8 +21,23 @@ def company_readout companies
 end 
 
 
-def all_sizes companies
-    companies.reduce do |total_size, company|
-        binding.pry
-    end
+def total_size companies
+    companies.reduce(0) { |total, company| total + company.size}
+end
+
+
+def companies_over_100 companies
+    companies.filter {|company| company.size > 100}
+end
+
+def company_named_beta companies
+    companies.find {|company| company.name == "Beta"}
+end
+
+def largest_company companies
+    companies.max_by {|company| company.size}
+end
+
+def sort companies
+    companies.reverse {|company| company.size}
 end
